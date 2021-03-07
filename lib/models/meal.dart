ea0a2738e8f30347390ffae7b56492a7a0e2b34b
Safwan <safwan9f@gmail.com>
2021-03-08 01:02:40 +0300

    Add the new dummy file, chnage the meal model, rename complexity enum

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum MealComplexity {
+enum Complexity {
   Simple,
   Challenging,
   Hard,
@@ -14,22 +14,22 @@ enum Affordability {
 
 class Meal {
   final String id;
-  final List<String> categoryIds;
+  final List<String> categories;
   final String title;
   final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
   final int duration;
-  final MealComplexity complexity;
+  final Complexity complexity;
   final Affordability affordability;
   final bool isGlutenFree;
   final bool isLactoseFree;
   final bool isVegan;
   final bool isVegetarian;
 
-  Meal({
+  const Meal({
     @required this.id,
-    @required this.categoryIds,
+    @required this.categories,
     @required this.title,
     @required this.imageUrl,
     @required this.ingredients,
