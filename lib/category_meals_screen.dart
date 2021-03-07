b912edbe6013d154a9ef8654f9dc3b17b03d2a79
Safwan <safwan9f@gmail.com>
2021-03-08 00:32:52 +0300

    Implement navigation between screens

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Category Name"),
+      ),
+      body: Text("Hello World"),
+    );
+  }
+}
