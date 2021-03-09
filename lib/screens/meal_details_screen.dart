2904281b50899365ae9a5876ec279ad531812598
Safwan <safwan9f@gmail.com>
2021-03-10 00:10:29 +0300

    Add steps with a divider in between, circlerAvatar, extract builder methods

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -43,37 +43,54 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          _buildSectionTitle(
-            context: context,
-            text: 'Ingredients',
-          ),
-          _buildSectionContainer(
-            child: ListView.builder(
-              itemBuilder: (context, index) => Card(
-                color: Theme.of(context).accentColor,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: Text(meal.ingredients[index]),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              itemCount: meal.ingredients.length,
             ),
-          ),
-          _buildSectionTitle(
-            context: context,
-            text: 'Steps',
-          ),
-        ],
+            _buildSectionTitle(
+              context: context,
+              text: 'Ingredients',
+            ),
+            _buildSectionContainer(
+              child: ListView.builder(
+                itemBuilder: (context, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                ),
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            _buildSectionTitle(
+              context: context,
+              text: 'Steps',
+            ),
+            _buildSectionContainer(
+                child: ListView.builder(
+              itemBuilder: (context, index) => Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${index + 1}'),
+                    ),
+                    title: Text(meal.steps[index]),
+                  ),
+                  Divider()
+                ],
+              ),
+              itemCount: meal.steps.length,
+            ))
+          ],
+        ),
       ),
     );
   }
