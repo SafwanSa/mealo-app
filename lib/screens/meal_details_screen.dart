d85bfcfc817680fd2f3339cdece108a56c24517c
Safwan <safwan9f@gmail.com>
2021-03-10 00:03:38 +0300

    Add the titles and the ingredianets to the MealDetailsScreen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -4,6 +4,32 @@ import 'package:mealo/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   static String routeName = '/meal-details';
+
+  Widget _buildSectionTitle({BuildContext context, String text}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _buildSectionContainer({@required Widget child}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 250,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final routeArgs =
@@ -26,6 +52,26 @@ class MealDetailsScreen extends StatelessWidget {
               meal.imageUrl,
               fit: BoxFit.cover,
             ),
+          ),
+          _buildSectionTitle(
+            context: context,
+            text: 'Ingredients',
+          ),
+          _buildSectionContainer(
+            child: ListView.builder(
+              itemBuilder: (context, index) => Card(
+                color: Theme.of(context).accentColor,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(meal.ingredients[index]),
+                ),
+              ),
+              itemCount: meal.ingredients.length,
+            ),
+          ),
+          _buildSectionTitle(
+            context: context,
+            text: 'Steps',
           ),
         ],
       ),
