f3c883462d91462a19d541f2c2c783ac989c4eb8
Safwan <safwan9f@gmail.com>
2021-03-10 00:23:32 +0300

    Use the TabsScreen widget to be loaded  as a home screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,25 +5,20 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mealo'),
-      ),
-      body: GridView(
-        padding: EdgeInsets.all(20),
-        children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(
-                  id: catData.id,
-                  title: catData.title,
-                  color: catData.color,
-                ))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-      ),
+    return GridView(
+      padding: EdgeInsets.all(20),
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(
+                id: catData.id,
+                title: catData.title,
+                color: catData.color,
+              ))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
     );
   }
 }
